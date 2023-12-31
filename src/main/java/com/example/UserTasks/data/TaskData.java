4bed6ebaa1f1c54902a6713760234c3c8bf4edee
Piotr Gazda <pjgazda@interia.pl>
2023-12-31 13:04:18 +0100

    Refactor data classes.

--- src/main/java/com/example/UserTasks/data/TaskData.java
+++ src/main/java/com/example/UserTasks/data/TaskData.java
@@ -1,18 +1,21 @@
 package com.example.UserTasks.data;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+
+import java.io.Serializable;
 
 /**
  * Data object for task related information
  */
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class TaskData {
+@Data
+public class TaskData implements Serializable {
+    @JsonProperty("title")
     private String title;
+    @JsonProperty("description")
     private String description;
+    @JsonProperty("status")
     private String status;
+    @JsonProperty("dueDate")
     private String dueDate;
 }
--- src/main/java/com/example/UserTasks/data/UserData.java
+++ src/main/java/com/example/UserTasks/data/UserData.java
@@ -1,17 +1,17 @@
 package com.example.UserTasks.data;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 /**
  * Data object for user related information
  */
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Data
 public class UserData {
+    @JsonProperty
     private String name;
+    @JsonProperty
     private String surname;
+    @JsonProperty
     private String email;
 }
